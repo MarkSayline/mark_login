66b08676057ab39b1eacb30b44c12ef537508383
MarkSayline <buryagumahom@gmail.com>
2022-09-16 13:21:38 +0300

    update
    fresh_update

---
+++ mark_login/Form1.cs
@@ -0,0 +1,69 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mark_login
+{
+    public partial class Form1 : Form
+    {
+        mark_db_connection con = new mark_db_connection();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtUsername.Text != "" && txtPassword.Text != "")
+                {
+
+                    con.Open();
+                    string query = "SELECT user_name,user_password FROM users WHERE user_name ='" + txtUsername.Text + "' AND user_password ='" + txtPassword.Text + "'";
+                    MySqlDataReader row;
+                    row = con.ExecuteReader(query);
+                    if (row.HasRows)
+                    {
+
+                        MessageBox.Show("Logged in Successfully......");
+                        this.Hide();
+                        Form3 landing_page = new Form3();
+                        landing_page.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Check Username and passwsord");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("All fields are required");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Connection Failed");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 register = new Form2();
+            register.Show();
+        }
+    }
+}
---
+++ mark_login/Form2.cs
@@ -0,0 +1,59 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mark_login
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 login = new Form1();
+            login.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string dbConn = "datasource=localhost;database=mark;username=root;password=";
+
+                string Query = "insert into users(user_name,user_password) values('" + this.txtUsername.Text + "','" + this.txtPassword.Text + "');";
+
+                MySqlConnection con = new MySqlConnection(dbConn);
+
+                MySqlCommand sqlCommand = new MySqlCommand(Query, con);
+
+                MySqlDataReader MyReader2;
+
+                con.Open();
+                MyReader2 = sqlCommand.ExecuteReader();
+                MessageBox.Show("Registration Successfull,use your username and password to login");
+                this.Hide();
+                Form1 login = new Form1();
+                login.Show();
+                while (MyReader2.Read())
+                {
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
